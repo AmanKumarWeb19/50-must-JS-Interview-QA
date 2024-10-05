ac61cbd0033c215332c47cf38018875c8357738f
Aman Kumar <akgupta0524@gmail.com>
2024-10-05 09:15:52 +0530

    question hash tag generator

---
+++ 02-Hash-Tag-Generator/index.js
@@ -0,0 +1,16 @@
+/**
+ * ----------------------------------------------------
+ * Programming Question : Hash Tag Generator.
+ * ----------------------------------------------------
+ */
+
+//? You are required to implement a function generateHashTag a hash tag from a given input String.
+//? The Hash tag should be constructed as follows:
+
+//? The input String should be converted to a hash tag format, where each word is capitalize and concatenated
+//? together without spaces.
+
+//? If the length of the input string is greater than 280 characters or if the input string is empty or contains
+//? only whitespace the function should return false.
+
+//? OtherWise , the function should return the generated hash tag prefixed with #.
